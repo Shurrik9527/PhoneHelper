9b05dcb66ed99b7b38281d514ca83e610a597ccc
heguogui <252774645@qq.com>
2018-09-06 21:02:14 +0800

    垃圾清理模块 进程管理 缓存管理

---
+++ app/src/main/java/com/jerrywang/phonehelper/manager/ProcessManager.java
@@ -0,0 +1,11 @@
+package com.jerrywang.phonehelper.manager;
+
+/**
+ * @author heguogui
+ * @version v 1.0.0
+ * @describe
+ * @date 2018/9/6
+ * @email 252774645@qq.com
+ */
+public class ProcessManager {
+}
